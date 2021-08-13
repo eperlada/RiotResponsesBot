3c92d3bfbf74ffff00d0247a5fda08772d39448c
Ethan Perlada <eperlada@ucsc.edu>
2021-08-12 18:50:36 -0700

    Include links to Riot employee's Reddit profile

--- reddit_bot.py
+++ reddit_bot.py
@@ -29,23 +29,20 @@ reddit = praw.Reddit(username=config["REDDIT"]["username"],
                      password=config["REDDIT"]["password"],
                      client_id=config["REDDIT"]["client_id"],
                      client_secret=config["REDDIT"]["client_secret"],
-                     user_agent="RiotResponses Bot v0.3")
+                     user_agent="RiotResponses Bot v0.4")
 reddit.validate_on_submit = True
 
 # Formats a comment with the author name, comment body, and permalink to the comment
-
-
 def formatResponse(comment):
     body = comment.body
     # Replace new lines with a single space to preserve Reddit table formatting
     body = re.sub(r'\n+', ' ', body).strip()
-    formatted = comment.author.name + \
+    formatted = '[' + comment.author.name + ']' + '(https://www.reddit.com/u/' + \
+        comment.author.name + ')' + \
         " | [Link](https://www.reddit.com" + comment.permalink + ") | " + body
     return formatted
 
 # Creates post with initial response
-
-
 def createPost(comment):
     # Post title format: [Original Poster] Submission Title
     if comment.submission.author == None:
@@ -54,7 +51,7 @@ def createPost(comment):
         op = comment.submission.author.name
     post_title = "[" + op + "] " + comment.submission.title
     body = "##[Original Post](" + comment.submission.permalink + ")"
-    body += "\n\nUsername | Link | Response\n" + \
+    body += "\n\nRiot Employee | Link | Response\n" + \
         "---|---|---\n" + formatResponse(comment)
     # Post to subreddit
     post = reddit.subreddit(config["REDDIT"]["target_subreddit"]).submit(
@@ -63,17 +60,19 @@ def createPost(comment):
     # Add submission ids for original post and response post to previous_posts
     previous_posts[comment.submission.id] = post.id
     # Update database
-    db.insert(comment.submission.id, post.id)
+    db.insertPost(comment.submission.id, post.id, op, comment.submission.title)
+    db.insertComment(comment.id, comment.submission.id, comment.author.name, comment.body_html)
 
 # Updates a previously created post to add a new response from the same submission
-
-
 def updatePost(comment, post):
     body = post.selftext
     body += "\n" + formatResponse(comment)
     post.edit(body)
+    # Update database
+    db.insertComment(comment.id, comment.submission.id, comment.author.name, comment.body_html)
 
 
+# Parse a comment from the subreddit stream
 def parseComment(comment):
     # Check if comment is from a Riot employee
     if comment.author_flair_text == ":riot:":
@@ -89,16 +88,18 @@ def parseComment(comment):
 dbcon = db.connect()
 previous_posts = {}
 
-# Check for table in database
+# Check for tables in database
 if not db.checkTableExists(dbcon, "Posts"):
-    db.createTable(dbcon)
-# If table already exists in database, import rows into dictionary
+    db.createPostsTable(dbcon)
+if not db.checkTableExists(dbcon, "Comments"):
+    db.createCommentsTable(dbcon)
+# If table already exists in database, load stored rows into memory
 else:
     dbcur = dbcon.cursor()
-    dbcur.execute("SELECT * FROM Posts")
+    dbcur.execute("SELECT sourceid, postid FROM Posts")
     dbposts = dbcur.fetchall()
-    for (source, post, datetime) in dbposts:
-        previous_posts[source] = post
+    for (sourceid, postid) in dbposts:
+        previous_posts[sourceid] = postid
 
 # Close database connection after loading in data
 db.disconnect(dbcon)
